e7b1be0d6fc9797fb0451bde864ed2ce26894e06
Hart Liddell <hart@leapingfish.io>
2016-08-05 16:59:47 -0400

    harts updates

--- js/project.js
+++ js/project.js
@@ -3,19 +3,17 @@ $(document).ready(function(){
   $("nav a").click(function(){
     $("nav a.current").removeClass("current");
     $(this).toggleClass("current");
-  });    
+  });
   $("button").click(function(){
     var section=$(".current").data("ipsum");
-    var number =$("#paragraphs").val();		
-    $(".ipsum").hide(); 
-    $("#"+section).find("p").slice(0,number).show();  
+    var number =$("#paragraphs").val();
+    $("#"+section).find("p").slice(0,number).show();
     $("#"+section).slideDown(500);
   });
-    $("#paragraphs").focus(function(){
+  $("#paragraphs").focus(function(){
     $("#paragraphs").val("");
-    $(".ipsum p").slideUp(500);
+    $(".ipsum").slideUp(500, function() {
+        $(this).find('p').hide();
+    });
   });
 });
-
-
-
